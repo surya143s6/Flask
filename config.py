e37ecdc4b953fdb8ee52af61b7f52f2164b92902
surya143s6 <suriyarangan2017@gmail.com>
2026-03-04 16:02:39 +0530

    env_setup

--- config.py
+++ config.py
@@ -1,6 +1,11 @@
+from os import environ
+from dotnev import load_dotenv
+
+load_dotenv()
+print(environ.get("DATABASE_URL"))
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = (
-        "postgresql://postgres:admin@localhost:5432/movies"
-    )
+    SQLALCHEMY_DATABASE_URI = environ.get("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
+    
